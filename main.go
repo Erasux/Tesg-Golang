4e4a8c405f7eb6f5d7ccaffe9a402820ff447fcd
Erasux <samirgg2000@gmail.com>
2025-03-31 12:09:27 -0500

    Update main.go to static values

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"SamirGG/Tesg-Golang/database"
 	"SamirGG/Tesg-Golang/handlers"
+	"SamirGG/Tesg-Golang/models"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func main() {
@@ -17,10 +20,18 @@ func main() {
 	}
 	defer database.DisconnectFromDatabase()
 
+	// Verificar si hay eventos y si no, crear datos iniciales
+	events, err := database.FindEvents()
+	if err != nil {
+		log.Printf("Error al verificar eventos: %v", err)
+	} else if len(events) == 0 {
+		fmt.Println("No hay eventos en la base de datos. Creando datos iniciales...")
+		seedDataBase()
+	}
+
 	router := gin.Default()
 
 	// Rutas de eventos
-
 	router.POST("/events", handlers.CreateEvent)
 	router.GET("/events", handlers.FindEvents)
 	router.GET("/events/:id", handlers.FindEventById)
@@ -32,4 +43,40 @@ func main() {
 		log.Fatal("Error al iniciar el servidor:", err)
 	}
 	fmt.Println("Servidor iniciado en el puerto 8080")
+}
+
+func seedDataBase() {
+	events := []models.Event{
+		{
+			ID:               primitive.NewObjectID(),
+			Name:             "Evento 1",
+			EventType:        "Tipo de evento 1",
+			Description:      "Descripción del evento 1",
+			Date:             time.Now(),
+			Status:           models.StatusPending,
+			ManagementStatus: models.ManagementUndefined,
+		},
+		{
+			ID:               primitive.NewObjectID(),
+			Name:             "Evento 2",
+			EventType:        "Tipo de evento 2",
+			Description:      "Descripción del evento 2",
+			Date:             time.Now().AddDate(0, 0, 1),
+			Status:           models.StatusPending,
+			ManagementStatus: models.ManagementUndefined,
+		},
+		{
+			ID:               primitive.NewObjectID(),
+			Name:             "Evento 3",
+			EventType:        "Tipo de evento 3",
+			Description:      "Descripción del evento 3",
+			Date:             time.Now().AddDate(0, 0, 2),
+			Status:           models.StatusPending,
+			ManagementStatus: models.ManagementUndefined,
+		},
+	}
+
+	for _, event := range events {
+		database.InsertEvent(event)
+	}
 }
