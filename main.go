d066576b961ce4514cc7bd852d005a4949de0043
Erasux <samirgg2000@gmail.com>
2025-03-31 11:42:32 -0500

    Create routes for events

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	router := gin.Default()
+
+	router.POST("/events")
+	router.GET("/events")
+	router.GET("/events/:id")
+	router.PUT("/events/:id")
+	router.DELETE("/events/:id")
+	router.Run(":8080")
 }
